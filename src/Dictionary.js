4edb68b0b967f9677605d28760102ef297853335
Nahid Norouzi <106868573+nefbao@users.noreply.github.com>
2022-08-02 07:09:35 +0430

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyword]=useState("");
+
+    function handleResponse(response){
+        console.log(response.data);
+    }
     function search(event){
         event.preventDefault();
-        alert(`searching for ${keyword}`);
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function changeKeword(event){
         setKeyword(event.target.value);
